59cdfbe73a8880638e65af4cfb53a1a825ee4c50
Thykof <thykof@protonmail.ch>
2021-04-17 18:47:19 +0200

    Add ERC1155 token contract

---
+++ contracts/erc1155/ERC1155Token.sol
@@ -0,0 +1,6 @@
+pragma solidity 0.6.2;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+
+contract ERC1155Token is ERC1155("") {}
---
+++ test/test_token.js
@@ -0,0 +1,23 @@
+const BigNumber = require('bignumber.js');
+const truffleAssert = require('truffle-assertions')
+const Web3 = require('web3')
+
+const web3 = new Web3(Web3.givenProvider)
+
+
+const ERC1155Token = artifacts.require('ERC1155Token')
+
+contract("ERC1155 Token", accounts => {
+
+  const [owner, shareholder] = accounts
+
+  before(async () => {
+    tokens = await ERC1155Token.new()
+  })
+
+  describe("Approval", async () => {
+    it("isApprovedForAll", async () => {
+      assert.equal(await tokens.isApprovedForAll(shareholder, owner), false)
+    })
+  })
+})
