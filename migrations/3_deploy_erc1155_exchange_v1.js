b6a4c06c9a7225fcc1ac132420bbebb6636ee86b
Thykof <thykof@protonmail.ch>
2021-04-18 11:08:20 +0200

    Deploy exchange implementation

---
+++ migrations/3_deploy_erc1155_exchange_v1.js
@@ -0,0 +1,5 @@
+const ERC1155ExchangeImplementationV1 = artifacts.require("ERC1155ExchangeImplementationV1");
+
+module.exports = function(deployer) {
+  deployer.deploy(ERC1155ExchangeImplementationV1); // gas used: 3502763
+};
